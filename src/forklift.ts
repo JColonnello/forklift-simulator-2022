5f49305ef6addb4a3e1ba91115d7d8b1e2ce722d
Alan Sartorio <alan42ga@hotmail.com>
2022-09-14 16:41:09 -0300

    Implemented realistic steering.

--- src/forklift.ts
+++ src/forklift.ts
@@ -1,4 +1,4 @@
-import { Vector2, Vector3 } from "three";
+import { Euler, Vector2, Vector3 } from "three";
 import { KeyManager } from "./keyManager";
 import { Key, Script } from "./script";
 
@@ -13,16 +13,17 @@ const grab: Key = "G";
 
 const minSpeed = 0.01;
 const maxSpeed = 3;
-const maxTurnSpeed = 4;
+const maxTurnSpeed = 0.5;
 const acceleration = 13;
 const drag = 10;
 
 export class ForkliftScript extends Script {
   keyManager?: KeyManager;
 
-  direction = new Vector2(1, 0);
+  angle = 0;
   position = new Vector3(0, 0, 0);
   speed = 0;
+  steeringPosition = 0;
 
   init(): void {
     this.keyManager = this.scriptManager.ofType<KeyManager>(KeyManager)!;
@@ -35,20 +36,25 @@ export class ForkliftScript extends Script {
   update(dt: number): void {
     let fwTrust = this.isKeyDown(forward) ? 1 : this.isKeyDown(back) ? -1 : 0;
 
-    let turnSpeed = this.isKeyDown(right) ? 1 : this.isKeyDown(left) ? -1 : 0;
+    let steeringPosition = this.isKeyDown(right) ? 1 : this.isKeyDown(left) ? -1 : 0;
+    this.steeringPosition += (steeringPosition - this.steeringPosition) * 0.1;
 
     this.speed += fwTrust * acceleration * dt;
-    if (Math.abs(this.speed) > maxSpeed) {
-      this.speed = Math.sign(this.speed) * maxSpeed;
-    }
     if (Math.abs(this.speed) > minSpeed) {
       this.speed -= Math.sign(this.speed) * drag * dt;
     } else {
       this.speed = 0;
     }
-    this.object.position.addScaledVector(new Vector3(this.direction.y, 0, -this.direction.x), this.speed * dt);
-    this.direction.rotateAround(new Vector2(), turnSpeed * maxTurnSpeed * dt);
-    this.object.rotation.set(0, -this.direction.angle(), 0)
+    if (Math.abs(this.speed) > maxSpeed) {
+      this.speed = Math.sign(this.speed) * maxSpeed;
+    }
+
+    this.angle += this.speed * this.steeringPosition * maxTurnSpeed * dt;
+
+    let direction = new Vector3(0, 0, -1);
+    direction.applyEuler(new Euler(0, -this.angle, 0))
+    this.object.position.addScaledVector(direction, this.speed * dt);
+    this.object.rotation.set(0, -this.angle, 0)
 
     this.object.position.clamp(new Vector3(-5, 0, -5), new Vector3(5, 0, 5))
   }
