def79e7d3dda90f136f864b90640716758ea291b
Alan Sartorio <alan42ga@hotmail.com>
2022-09-20 11:29:56 -0300

    Configured deploy to github pages.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:JColonnello/forklift-simulator-2022.git main:gh-pages
+
+cd -
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+  base: "/forklift-simulator-2022/",
+};
