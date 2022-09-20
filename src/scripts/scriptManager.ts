ee32e6b65c2676359b77dae3d74465ecb21fcd28
Alan Sartorio <alan42ga@hotmail.com>
2022-09-20 09:04:25 -0300

    Removed call to dispatch method.

--- src/scripts/scriptManager.ts
+++ src/scripts/scriptManager.ts
@@ -65,59 +65,31 @@ export class ScriptManager {
     });
   }
 
-  #dispatch(event: keyof Script, args: any[] = []) {
-    for (let script of this.scripts) {
-      switch (event) {
-        case 'update':
-          script.update(args[0]);
-          break;
-        case 'init':
-          script.init();
-          break;
-        case 'keydown':
-          script.keydown(args[0])
-          break;
-        case 'keyup':
-          script.keyup(args[0])
-          break;
-        case 'pointerdown':
-          script.pointerdown(args[0], args[1]);
-          break;
-        case 'pointerup':
-          script.pointerup(args[0], args[1]);
-          break;
-        case 'pointermove':
-          script.pointermove(args[0], args[1]);
-          break;
-      }
-    }
-  }
-
   dispatchUpdate(dt: number) {
-    this.#dispatch("update", [dt]);
+    this.scripts.forEach(s => s.update(dt));
   }
 
   dispatchPointerDown(x: number, y: number) {
-    this.#dispatch("pointerdown", [x, y]);
+    this.scripts.forEach(s => s.pointerdown(x, y));
   }
 
   dispatchPointerUp(x: number, y: number) {
-    this.#dispatch("pointerup", [x, y]);
+    this.scripts.forEach(s => s.pointerup(x, y));
   }
 
   dispatchPointerMove(x: number, y: number) {
-    this.#dispatch("pointermove", [x, y]);
+    this.scripts.forEach(s => s.pointermove(x, y));
   }
 
   dispatchKeyDown(key: Key) {
-    this.#dispatch("keydown", [key]);
+    this.scripts.forEach(s => s.keydown(key));
   }
 
   dispatchKeyUp(key: Key) {
-    this.#dispatch("keyup", [key]);
+    this.scripts.forEach(s => s.keyup(key));
   }
 
   dispatchInit() {
-    this.#dispatch("init");
+    this.scripts.forEach(s => s.init());
   }
 }
