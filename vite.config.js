6379301342e3c6fa105f8008030b71674d1f1675
Alan Sartorio <alan42ga@hotmail.com>
2022-10-11 21:13:12 -0300

    Disabled bundling of three and dat.gui into index.js.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/forklift-simulator-2022/",
   //base: "",
-};
+  build: {
+    minify: false,
+    rollupOptions: {
+      external: [
+        /three.*/,
+        "dat.gui",
+      ]
+    }
+  }
+});
